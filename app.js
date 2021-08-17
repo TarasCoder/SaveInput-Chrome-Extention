1d79bebb852ec3dcee9487b82769f3f8b8623771
kaskad <kaskad@bigmir.net>
2021-08-17 23:40:59 +0300

    ready to deploy to google chrome - no local storage so far"

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 let myLeads = [];
+let url = "";
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const urlBtn = document.getElementById("url-btn");
+
+urlBtn.addEventListener("click", getUrl);
 
 inputBtn.addEventListener("click", function (ev) {
   ev.preventDefault();
@@ -9,7 +13,17 @@ inputBtn.addEventListener("click", function (ev) {
   renderLeads();
 });
 
+function getUrl(ev) {
+  ev.preventDefault();
+  url = window.location.href;
+  myLeads.push(url);
+  renderLeads();
+  console.log(myLeads)
+}
+
 function renderLeads() {
+  // option 1*********
+
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
     listItems += `
@@ -22,4 +36,19 @@ function renderLeads() {
   }
   ulEl.innerHTML = listItems;
   inputEl.value = "";
+
+  // option 2******************
+  // let li = document.createElement("li");
+  // li.textContent = inputEl.value;
+  // ulEl.append(li);
+  // inputEl.value = "";
+  // console.log(myLeads);
+
+  // option 3*******************
+  // let li = document.createElement("li");
+  // for (let i = 0; i < myLeads.length; i++) {
+  //   li.textContent = myLeads[i];
+  //   ulEl.append(li);
+  //   inputEl.value = "";
+  // }
 }
